74405812c5ee00ef5a57a2cc60e4f26b01ab0039
Bitnut <940095072@qq.com>
2017-08-28 21:24:10 +0800

    moved all apis for composer to this version; still cannot run since tool bug of log4js; add routers for apis; eslint rule for 'no labels' problem havent solved yet

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,10 +3,49 @@ const authD = require('../controllers/doctorSign')
 const infoOfDoc = require('../controllers/doctorDashbord')
 const router = require('koa-router')()
 
+var network = require('../models/network.js')
+var channels = require('../models/createChannel.js')
+var join = require('../models/joinchannel.js')
+var install = require('../models/installChaincode.js')
+var instantiate = require('../models/instantiateChaincode.js')
+var invoke = require('../models/invokeTransaction.js')
+var query = require('../models/query.js')
+
+// For admin to build connect
 router.get('/admin', authA.buildConnect)
+// For admin to login
 router.post('/admin', authA.postAdminAuth)
+// For doctor to build connect
 router.get('/doctor', authD.buildConnect)
+// For doctor to login
 router.post('/doctor', authD.postDoctorAuth)
+// Load in doctorDashboard
 router.get('/doctorDash/:id', infoOfDoc.getMedicalLogs, infoOfDoc.getTransactions)
+// Register and enroll user
+router.post('/users', network.getRegisteredUsers)
+// Create Channel
+router.post('/channels', channels.createChannel)
+// Join Channel
+router.post('/channels/:channelName/peers', join.joinChannel)
+// Install chaincode on target peers
+router.post('/chaincodes', install.installChaincode)
+// Instantiate chaincode on target peers
+router.post('/channels/:channelName/chaincodes', instantiate.instantiateChaincode)
+// Invoke transaction on chaincode on target peers
+router.post('/channels/:channelName/chaincodes/:chaincodeName', invoke.invokeChaincode)
+// Query on chaincode on target peers
+router.get('/channels/:channelName/chaincodes/:chaincodeName', query.queryChaincode)
+//  Query Get Block by BlockNumber
+router.get('/channels/:channelName/blocks/:blockId', query.getBlockByNumber)
+// Query Get Transaction by Transaction ID
+router.get('/channels/:channelName/transactions/:trxnId', query.getTransactionByID)
+// Query Get Block by Hash
+router.get('/channels/:channelName/blocks', query.getBlockByHash)
+// Query for Channel Information
+router.get('/channels/:channelName', query.getChainInfo)
+// Query to fetch all Installed/instantiated chaincodes
+router.get('/chaincodes', query.getInstalledChaincodes)
+// Query to fetch channels
+router.get('/channels', query.getChannels)
 
 module.exports = router
