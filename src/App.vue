9f5fcd47339e8304b5d6f9799d089638d45de286
zephaniacn <zephaniacn@outlook.com>
2017-08-28 14:45:19 +0800

    增加导航

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <Nav></Nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Nav from './components/Nav'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Nav
+  }
 }
 </script>
 
