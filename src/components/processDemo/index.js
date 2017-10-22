dcf6ffbcb48862a187daed6a8ce16d06ad5d0d2b
Kyon <kyonGuo@163.com>
2017-10-23 00:58:56 +0800

    add processDemo

---
+++ src/components/processDemo/index.js
@@ -0,0 +1,7 @@
+import ProcessDemo from './ProcessDemo.vue'
+const ProcessDemo={
+    install:function (Vue) {
+        Vue.component('ProcessDemo',ProcessDemo)
+    }
+};
+export default ProcessDemo
